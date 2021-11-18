390fb93f4cb9fdc9965e9e4c1ed53a9cff04d27d
Aki <uenoaki97@gmail.com>
2021-11-19 00:00:45 +0100

    test event handling

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Card } from "./components/Card/Card";
 
-export const App = () => (
+export const App = () => {
+  return (
   <div className="main">
-    <Card title="Test">Kind</Card>
-    <div className="user info">Hallo Nutzer</div>
+    <Card title="Test">This is a card component</Card>
+    <button onClick={onButtonClickHandler}>Click here!</button>
+    <input type="text" onChange={onInputChangeHandler} />
   </div>
-);
+  )
+};
+
+function onButtonClickHandler() {
+  console.log("clicked");
+}
+
+function onInputChangeHandler(e: React.ChangeEvent<HTMLInputElement>){
+  console.log(e.target.value);
+}
